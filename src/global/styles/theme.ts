67d50036267360217a9027a14b0637066b082b5a
cassohir <cassio.hir@gmail.com>
2022-11-29 12:13:23 -0300

    changing theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,7 +4,7 @@ export const lightTheme: DefaultTheme = {
   colors: {
     background: 'hsl(250, 60%, 95%)',
     headerBackground: 'hsl(250, 60%, 95%)',
-    sidebarBackground: '#490ECD',
+    sidebarBackground: 'hsl(240, 6%, 12%)',
     skillBackground: 'hsl(250, 50%, 90%)',
     btnTopBackground: 'hsl(250, 50%, 85%)',
     inputBackground: 'hsl(250, 60%, 94%)',
@@ -13,9 +13,9 @@ export const lightTheme: DefaultTheme = {
     title: '#111',
     titleSecondary: '#222222CC',
     dataTimeText: '#222222AA',
-    primary: '#490ECD',
-    secondary: '#915EFF', //B330EA
-    themeIcon: '#490ECD',
+    primary: 'hsl(240, 6%, 13%)',
+    secondary: 'hsl(240, 6%, 14%)', //B330EA
+    themeIcon: '#5FFA15',
     backgroundThemeIcon: '#22222215',
     backgroundThemeIcon2: '#22222208',
     filterIcon: 'brightness(0.8)',
@@ -25,23 +25,61 @@ export const lightTheme: DefaultTheme = {
 
 export const darkTheme: DefaultTheme = {
   colors: {
-    background: 'hsl(250, 28%, 12%)',
-    headerBackground: 'hsl(250, 28%, 12%)',
-    sidebarBackground: '#D9A404',
-    skillBackground: 'hsl(250, 28%, 20%)',
-    btnTopBackground: 'hsl(250, 28%, 25%)',
-    inputBackground: 'hsl(250, 28%, 15%)',
+    background: 'hsl(240, 6%, 13%)',
+    headerBackground: 'hsl(240, 6%, 13%)',
+    sidebarBackground: '#259A38',
+    skillBackground: 'hsl(240, 6%, 14%)',
+    btnTopBackground: 'hsl(240, 6%, 14%)',
+    inputBackground: 'hsl(240, 6%, 14%)',
     headerText: '#fefefe',
     text: '#fefefe',
     title: '#fff',
     titleSecondary: '#ddddddCC',
     dataTimeText: '#dddddd99',
-    primary: '#D9A404',
-    secondary: '#F2CB05', // 
-    themeIcon:'#D9A404',
+    primary: '#259A38',
+    secondary: '#269912', // 
+    themeIcon:'#259A38',
     backgroundThemeIcon: '#dddddd22',
     backgroundThemeIcon2: '#dddddd11',
     filterIcon: 'brightness(1.2)',
     filterText: 'brightness(0.8)'
   }
 }
+
+  // background: 'hsl(250, 28%, 12%)',
+  //   headerBackground: 'hsl(250, 28%, 12%)',
+  //   sidebarBackground: '#D9A404',
+  //   skillBackground: 'hsl(250, 28%, 20%)',
+  //   btnTopBackground: 'hsl(250, 28%, 25%)',
+  //   inputBackground: 'hsl(250, 28%, 15%)',
+  //   headerText: '#fefefe',
+  //   text: '#fefefe',
+  //   title: '#fff',
+  //   titleSecondary: '#ddddddCC',
+  //   dataTimeText: '#dddddd99',
+  //   primary: '#D9A404',
+  //   secondary: '#F2CB05', // 
+  //   themeIcon:'#D9A404',
+  //   backgroundThemeIcon: '#dddddd22',
+  //   backgroundThemeIcon2: '#dddddd11',
+  //   filterIcon: 'brightness(1.2)',
+  //   filterText: 'brightness(0.8)'
+  
+    // background: 'hsl(250, 60%, 95%)',
+    // headerBackground: 'hsl(250, 60%, 95%)',
+    // sidebarBackground: '#490ECD',
+    // skillBackground: 'hsl(250, 50%, 90%)',
+    // btnTopBackground: 'hsl(250, 50%, 85%)',
+    // inputBackground: 'hsl(250, 60%, 94%)',
+    // headerText: '#222',
+    // text: '#222',
+    // title: '#111',
+    // titleSecondary: '#222222CC',
+    // dataTimeText: '#222222AA',
+    // primary: '#490ECD',
+    // secondary: '#915EFF', //B330EA
+    // themeIcon: '#490ECD',
+    // backgroundThemeIcon: '#22222215',
+    // backgroundThemeIcon2: '#22222208',
+    // filterIcon: 'brightness(0.8)',
+    // filterText: 'brightness(1.2)'
